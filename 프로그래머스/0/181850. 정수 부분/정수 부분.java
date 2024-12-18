e0759040333f243a1e52ba74d7ea8486067e7c8a
BangYongSu <dragonwater0417@gmail.com>
2024-12-19 00:14:42 +0900

    [level 0] Title: 정수 부분, Time: 0.03 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181850. 정수 부분/정수 부분.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(double flo) {
+        int answer = 0;
+      for(int i=0; i<=flo; i++){
+          answer=i;
+      }
+        
+        return answer;
+    }
+}
